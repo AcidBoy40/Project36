c029d292a3b7f3a4f706a24cbbbd13aa6d3bc014
AcidBoy40 <73644371+AcidBoy40@users.noreply.github.com>
2021-11-24 15:00:13 +0530

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,58 @@
+class Food{
+
+    constructor(){
+        this.foodStock = 0;
+        this.lastFed;
+        this.image = loadImage("Milk.png");
+    }
+    
+    getFoodStock(){
+        return this.foodStock();
+    }
+
+    updateFoodStock(foodStock){
+        this.foodStock = foodStock;
+    }
+
+    deductFood(){
+        if(this.foodStock > 0){
+            this.foodStock = this.foodStock - 1;
+        }
+    }
+
+    getFedTime(lastFed){
+        this.lastFed = lastFed;
+    }
+    
+    display(){
+        
+        var x = 80, y = 100;
+
+        imageMode(CENTER);
+        // image(this.image, 720, 220, 70, 70);
+
+        if(this.foodStock != 0){
+            for(var m = 0; m < this.foodStock; m++){
+                if(m % 10 === 0){
+                    x = 80;
+                    y = y + 50;
+                }
+                image(this.image, x, y, 50, 50);
+                x = x + 30;
+            }
+        }
+    }
+
+    livingroom(){
+        background(livingImage, 550, 500);
+    }
+
+    garden(){
+        background(gardenImage, 550, 500);
+    }
+
+    washroom(){
+        background(washroomImage, 550, 500);
+    }
+
+}
